689081bde86ada4572b05ce237e2efae17d9600d
marimo123-sudo <galievr323@gmail.com>
2025-05-05 07:52:34 +0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    Telegram.WebApp.setHeaderColor("#ff00c8");
-    Telegram.WebApp.setBackgroundColor("#ff00c8");
-    const heit = window.innerHeight;
-    console.log(heit);
-    
-    document.documentElement.style.setProperty('--viewport-height', `${heit}px`);
-});
