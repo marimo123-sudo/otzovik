497b91825e85b833400d81bcc62142a534b3dd01
marimo123-sudo <galievr323@gmail.com>
2025-05-05 12:13:45 +0300

    Add files via upload

---
+++ profile/bottom.js
@@ -0,0 +1,14 @@
+const search_btn = document.getElementById("search")
+const profile_btn = document.getElementById("profile")
+
+
+profile_btn.addEventListener("click", () => {
+    console.log(1);
+    window.location.href = 'profile.html';
+});
+
+
+search_btn.addEventListener("click", () => {
+    console.log(1);
+    window.location.href = '../index.html';
+});
