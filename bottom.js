38153c81dab6ec968343584e9bee6c9cf0902d02
marimo123-sudo <galievr323@gmail.com>
2025-05-05 12:09:35 +0300

    Update bottom.js

--- bottom.js
+++ bottom.js
@@ -4,11 +4,11 @@ const profile_btn = document.getElementById("profile")
 
 profile_btn.addEventListener("click", () => {
     console.log(1);
-    window.location.href = '/profile/profile.html';
+    window.location.href = 'profile/profile.html';
 });
 
 
 search_btn.addEventListener("click", () => {
     console.log(1);
-    window.location.href = '/index.html';
+    window.location.href = 'index.html';
 });
